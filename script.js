b8cb07a505b1acd7003d98a94b073ecc1dacf044
Jiaraee <119480346+Jiaraee@users.noreply.github.com>
2024-09-18 13:14:50 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("custom-download-button").onclick = function() {
+        alert("Button clicked! Functionality will be added later.");
+    };
+});
